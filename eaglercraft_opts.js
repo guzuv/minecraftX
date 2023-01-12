f0812c58a3ad34f91c2f6dcc1a1de751395e00fd
guzuv <110692178+guzuv@users.noreply.github.com>
2023-01-12 09:46:37 -0500

    Update eaglercraft_opts.js

--- eaglercraft_opts.js
+++ eaglercraft_opts.js
@@ -3,7 +3,7 @@ window.eaglercraftXOpts = () => ({
   // joinServer: "ws" + window.location.href.slice(4),
   // assetsURI: "custom.epk",
 	servers: [
-		{ addr: "ws" + window.location.href.slice(4), name: "guzuv's school server!" }
+		{ addr: "mc.guzuv.repl.co", name: "guzuv's school server!" }
 	]
 });
 // document.title = "My Original Server Name:tm:";
